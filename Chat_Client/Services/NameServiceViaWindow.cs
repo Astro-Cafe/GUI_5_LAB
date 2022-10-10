51416e27558b24fa3288e2ef8d5ba5c4e1a4c4de
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-10-10 16:45:04 +0200

    NameWindowFixed

--- Chat_Client/Services/NameServiceViaWindow.cs
+++ Chat_Client/Services/NameServiceViaWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chat_Client.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,9 @@ namespace Chat_Client.Services
 	{
 		public string GetName()
 		{
-			return null;
+			NameWindow nw = new NameWindow();
+			nw.ShowDialog();
+			return nw.VM.EnteredName;
 		}
 	}
 }
