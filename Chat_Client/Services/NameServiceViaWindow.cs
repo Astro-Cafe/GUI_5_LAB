91dc1f04a08c99e670f62aee03b5ab517f7b9916
ichiroutaa <gergely.tovizi@gmail.com>
2022-10-10 16:45:54 +0200

    Other

--- Chat_Client/Services/NameServiceViaWindow.cs
+++ Chat_Client/Services/NameServiceViaWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chat_Client.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,10 @@ namespace Chat_Client.Services
 {
 	public class NameServiceViaWindow : INameService
 	{
-		public string GetName()
-		{
-			return null;
-		}
-	}
+        public string GetName()
+        {
+            return null;
+        }
+    }
 }
+
