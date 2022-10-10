d3d04e4658da4bdfcecd4947a3f8a802bc9b8090
ichiroutaa <gergely.tovizi@gmail.com>
2022-10-10 15:59:16 +0200

    MessageController added

---
+++ Chat_Endpoint/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Chat_Endpoint.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat_Endpoint.Controllers
+{
+    public class MessageController
+    {
+        IHubContext<SignalRHub> hub;
+
+        public MessageController(IHubContext<SignalRHub> hub)
+        {
+            this.hub = hub;
+        }
+    }
+}
