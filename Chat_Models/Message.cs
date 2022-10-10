158af232e5b7f40a63aadce7183f6e5a63ccfff4
ichiroutaa <gergely.tovizi@gmail.com>
2022-10-10 15:53:14 +0200

    Message class created

---
+++ Chat_Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Models
+{
+    public class Message
+    {
+        public string Sender { get; set; }
+        public DateTime SendDate { get; set; } = DateTime.Now;
+        public string SentMessage { get; set; }
+    }
+}
